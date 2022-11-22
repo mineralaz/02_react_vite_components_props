cd2f3602308e62e80f0b9cfe5f1ab3a208ec77af
mineralaz <jakimaviciuspovilas@gmail.com>
2022-11-22 09:45:32 +0200

    service 3 card

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,37 @@
 import './App.css';
 
 function SectionTitle(props) {
-  console.log('props ===', props);
+  // console.log('props ===', props);
   return <h2 className="sectionTitle">{props.title}</h2>;
 }
 
 function Icon(props) {
+  // console.log('props ===', props);
   return <i className={`fa ${props.iconName}`} aria-hidden="true"></i>;
 }
 
 function Grid() {
   return (
     <div className="grid">
-      <h2>
-        koretele <Icon iconName="fa-rocket" />
-      </h2>
-      <h2>
-        koretele <Icon iconName="fa-desktop" />
-      </h2>
-      <h2>
-        koretele <Icon iconName="fa-wrench" />
-      </h2>
+      <ServiceCard />
+      <ServiceCard />
+      <ServiceCard />
+    </div>
+  );
+}
+
+function ServiceCard() {
+  return (
+    <div className="card">
+      <Icon className="card__icon" iconName="fa-rocket" />
+      <h3>title</h3>
+      <p className="card__text">
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur,
+        suscipit?
+      </p>
+      <a href="#" className="card__link">
+        Learn more
+      </a>
     </div>
   );
 }
