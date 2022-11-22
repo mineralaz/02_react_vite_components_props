e13ea10c10c5190f863b2b9deeaecc9c064a0a49
mineralaz <jakimaviciuspovilas@gmail.com>
2022-11-22 09:30:19 +0200

    props

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
 import './App.css';
+
+function SectionTitle(props) {
+  return <h2 className="sectionTitle">Our Services</h2>;
+}
+
+function Icon() {
+  return <i class="fa fa-rocket" aria-hidden="true"></i>;
+}
+
+function Grid() {
+  return (
+    <div className="grid">
+      <h2>
+        koretele <Icon />
+      </h2>
+      <h2>
+        koretele <Icon />
+      </h2>
+      <h2>
+        koretele <Icon />
+      </h2>
+    </div>
+  );
+}
 
 function App() {
   return (
     <div className="App container">
-      <h1>Hello vite react</h1>
+      <SectionTitle title="Musu paslaugos" />
+      <Grid />
     </div>
   );
 }
