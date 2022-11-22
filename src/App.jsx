c3e9acbb943027e8055401062fd12d080f7ece6e
mineralaz <jakimaviciuspovilas@gmail.com>
2022-11-22 10:20:30 +0200

    OurServices

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,32 @@ function SectionTitle(props) {
 
 function Icon(props) {
   // console.log('props ===', props);
-  return <i className={`fa ${props.iconName} fa-3x`} aria-hidden="true"></i>;
+  return (
+    <i
+      className={`fa ${props.iconName} fa-3x card__icon`}
+      aria-hidden="true"
+    ></i>
+  );
 }
 
 function Grid() {
   return (
     <div className="grid">
+      <ServiceCard
+        title="Business consulting"
+        icon="fa-rocket"
+        link="/about.html"
+      />
+      <ServiceCard
+        title="Market Analysis"
+        icon="fa-desktop"
+        link="/about.html"
+      />
+      <ServiceCard
+        title="User Monitoring"
+        icon="fa-wrench"
+        link="/about.html"
+      />
       <ServiceCard
         title="Business consulting"
         icon="fa-rocket"
@@ -35,7 +55,7 @@ function Grid() {
 function ServiceCard(props) {
   return (
     <div className="card">
-      <Icon className="card__icon" iconName={props.icon} />
+      <Icon iconName={props.icon} />
       <h3>{props.title}</h3>
       <p className="card__text">
         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tenetur,
@@ -48,12 +68,20 @@ function ServiceCard(props) {
   );
 }
 
-function App() {
+function OurServices() {
   return (
-    <div className="App container">
+    <div className="servicesSection">
       <SectionTitle title="Musu paslaugos" size="large" />
       <Grid />
       {/* <SectionTitle title="About us" /> */}
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <div className="App container">
+      <OurServices />
     </div>
   );
 }
